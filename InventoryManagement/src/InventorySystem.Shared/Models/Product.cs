7675dcb53ff31c136430532a5c3eac11d72411da
mari <mita0001@ce.pucmm.edu.do>
2026-06-07 01:20:52 -0400

    6.1 - adding visuals and a bit of functionality for CRUD Not completed, just a start.

---
+++ InventoryManagement/src/InventorySystem.Shared/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace InventorySystem.Shared;
+    public class Product
+    {
+        public string? Name { get; set; }
+        public string? CodeSKU { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int MinimumStockLevel { get; set; }
+        public bool IsActive { get; set; }
+    }
