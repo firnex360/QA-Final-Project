f69c3d996a66d10e3c857e78ed329b9bb99fdaeb
firnex <fernandorodriguezb27@gmail.com>
2026-06-11 11:49:50 -0400

    removed reference to product service on client
    this need to be modified to use api calls instead

--- InventoryManagement/src/InventorySystem.Client/Program.cs
+++ InventoryManagement/src/InventorySystem.Client/Program.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using InventorySystem.Client;
-using InventorySystem.Shared.Services;
-
+using InventorySystem.Shared.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddSingleton<ProductService>();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<Product>();
 
 await builder.Build().RunAsync();
