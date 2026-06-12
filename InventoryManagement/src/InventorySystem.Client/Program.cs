5403830312c9b167e378ddae896b1c05e711b7ea
Maria Isabel Tiburcio Arias <mita0001@ce.pucmm.edu.do>
2026-06-12 14:36:33 -0400

    6 crud visuals (#15)
    * 6.1 - adding visuals and a bit of functionality for CRUD
    Not completed, just a start.
    
    * [DO NOT MERGE] attempting keycloak w/ blazor. login works, logout needs review
    
    * DO NOT MERGE - this is a test for keycloak with blazor server, not the final implementation for the client project.
    
    * clean scope

--- InventoryManagement/src/InventorySystem.Client/Program.cs
+++ InventoryManagement/src/InventorySystem.Client/Program.cs
@@ -7,6 +7,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Keycloak", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Clear();
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+});
+
+builder.Services.AddAuthorizationCore();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<Product>();
