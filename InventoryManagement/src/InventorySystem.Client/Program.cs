a00dc543c45a0bd3204e400ff40da9df531617d0
firnex <fernandorodriguezb27@gmail.com>
2026-06-12 17:37:46 -0400

    fix error with baseurl
    the base url was pointing to another pro

--- InventoryManagement/src/InventorySystem.Client/Program.cs
+++ InventoryManagement/src/InventorySystem.Client/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using InventorySystem.Client;
-using InventorySystem.Shared.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -20,9 +19,5 @@ builder.Services.AddOidcAuthentication(options =>
 });
 
 builder.Services.AddAuthorizationCore();
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddSingleton<Product>();
 
 await builder.Build().RunAsync();
