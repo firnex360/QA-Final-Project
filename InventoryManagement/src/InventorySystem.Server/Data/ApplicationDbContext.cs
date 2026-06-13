faef4284de5913c5db54e36c26cfeb00d4b03c23
Firnex <98984614+firnex360@users.noreply.github.com>
2026-06-13 16:59:12 -0400

    20 implement audit (#22)
    * added auditing using Audit .NET
    
    * updated the DB to add the table for audits
    
    * change some semantics
    
    * added https redirection only for production
    
    doesnt have anything to with this issue

--- InventoryManagement/src/InventorySystem.Server/Data/ApplicationDbContext.cs
+++ InventoryManagement/src/InventorySystem.Server/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
+using Audit.EntityFramework;
+using InventorySystem.Server.Models;
 using InventorySystem.Shared.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace InventorySystem.Server.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : AuditDbContext(options)
 {
-
     public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<AuditLog> AuditLogs { get; set; } = null!;
 }
